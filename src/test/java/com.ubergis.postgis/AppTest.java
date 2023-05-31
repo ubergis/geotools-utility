aeb5c9927c38ab9362db27a9d302e74b39f31c45
ShuiYuan Zhong <85913073+ubergis@users.noreply.github.com>
2023-05-31 09:20:01 +0800

    commit test
    commit test

---
+++ src/test/java/com.ubergis.postgis/AppTest.java
@@ -0,0 +1,17 @@
+package com.ubergis.postgis;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
